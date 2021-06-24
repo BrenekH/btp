cff3b223d804659cc9889605759a200d1bd53d03
Brenek Harrison <brenekharrison@gmail.com>
2021-06-23 20:05:09 -0600

    Working CLI

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log("Thank you for taking a look at BTP. Unfortunately, there is no functionality to speak of. If you want to help add it, checkout github.com/BrenekH/btp. You may also be interested in checking out the main project, Beryl (github.com/BrenekH/beryl).")
