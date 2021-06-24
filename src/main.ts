c6bfad5a5c9df2b0ad6357eb992c26893e283cf4
Brenek Harrison <brenekharrison@gmail.com>
2021-06-23 21:00:11 -0600

    Simple package command

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+import { Command } from "commander"
 
-console.log("Thank you for taking a look at BTP. Unfortunately, there is no functionality to speak of. If you want to help add it, checkout github.com/BrenekH/btp. You may also be interested in checking out the main project, Beryl (github.com/BrenekH/beryl).")
+import { packageBIPA } from "./package"
+
+console.log("Thank you for taking a look at BTP. Unfortunately, there is no functionality to speak of. If you want to help add it, checkout github.com/BrenekH/btp. You may also be interested in checking out the main project, Beryl (github.com/BrenekH/beryl).\n\n")
+
+const program = new Command()
+
+program.command("package").description("package current directory into a Beryl plugin").action(packageBIPA)
+
+program.parse(process.argv)
