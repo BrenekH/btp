6eb087dd76d2e4d787a331d1e02888fbce2b2432
Brenek Harrison <brenekharrison@gmail.com>
2021-06-23 21:24:49 -0600

    Read and filter out files in directory

--- src/package.ts
+++ src/package.ts
@@ -1,3 +1,28 @@
-export function packageBIPA() {
+import { readdirSync, readFileSync, statSync } from "fs"
+import ignore from "ignore"
+import { join } from "path"
 
+export function packageBIPA() {
+	const filesToAdd = ignore().add(readFileSync("./.berylignore").toString()).filter(getAllFiles("."))
+
+	console.log(filesToAdd)
+}
+
+function getAllFiles(dirPath: string, passedArrayOfFiles?: string[]): string[] {
+	let files = readdirSync(dirPath)
+
+	let arrayOfFiles: string[] = []
+	if (passedArrayOfFiles !== undefined) {
+		arrayOfFiles = passedArrayOfFiles
+	}
+
+	files.forEach(function(file) {
+		if (statSync(dirPath + "/" + file).isDirectory()) {
+			arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+		} else {
+			arrayOfFiles.push(join(dirPath, "/", file))
+	}
+	})
+
+	return arrayOfFiles
 }
