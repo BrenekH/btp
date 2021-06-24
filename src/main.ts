f46b6550dfa8a20483b55052ce636c7a1198909c
Brenek Harrison <brenekharrison@gmail.com>
2021-06-23 21:52:20 -0600

    Remove functionality warning

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { Command } from "commander"
 
 import { packageBIPA } from "./package"
 
-console.log("Thank you for taking a look at BTP. Unfortunately, there is no functionality to speak of. If you want to help add it, checkout github.com/BrenekH/btp. You may also be interested in checking out the main project, Beryl (github.com/BrenekH/beryl).\n\n")
-
 const program = new Command()
 
 program.command("package").description("package current directory into a Beryl plugin").action(packageBIPA)
